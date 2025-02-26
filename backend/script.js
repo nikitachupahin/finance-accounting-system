54cea4c2a0d55a129d875d10bf951998e36de144
Nikita Chupahin <nik123chupahin@gmail.com>
2025-02-26 16:52:02 +0200

    Added dependencies and modules

---
+++ backend/script.js
@@ -0,0 +1,14 @@
+// CREATE TABLE users (
+// 	id SERIAL NOT NULL PRIMARY KEY,
+// 	email VARCHAR(120) UNIQUE NOT NULL,
+// 	name VARCHAR(50) NOT NULL,
+// 	password TEXT,
+// 	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+// );
+
+// INSERT INTO users 
+// 	(email, name, password)
+// VALUES
+// 	('nikwerwer@gmail.com', 'Bob', '1234');
+
+// SELECT * FROM users;
